c4a88612e9dbf005b68bc8ff27e42ff750016bf0
Bujenita Claudiu <bujenitaclaudiu@gmail.com>
2023-12-08 15:21:44 +0200

    create new build and add pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'test-lisa';
+  installPrompt!: Event;
+
+  constructor() {
+    window.addEventListener('beforeinstallprompt', (event) => {
+      event.preventDefault();
+      this.installPrompt = event;
+    });
+  }
+
+  onInstallClick() {
+    if (this.installPrompt) {
+      (this.installPrompt as any).prompt();
+    } else {
+      alert('Install prompt is not available');
+    }
+  }
 }
